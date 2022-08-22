527b4fbbfa3f3ae2896fa5ff237293b301e2a089
이주연 <45332354+1uyeon@users.noreply.github.com>
2022-08-22 15:11:22 +0900

    a 태그 닫힘 수정

--- js/main.js
+++ js/main.js
@@ -90,10 +90,10 @@ $(document).ready(function() {
     $(this).stop().animate({"top":0, "opacity":1});
   });
   $('.skip a').on('click', function(){
-    $(this).stop().animate({"top":"-30px", "opacity":0});
+    $(this).stop().animate({"top":"-105px", "opacity":0});
   });
   $('.skip a').on('focusout', function(){
-      $(this).stop().animate({"top":"-30px", "opacity":0});
+    $(this).stop().animate({"top":"-105px", "opacity":0});
   });
 });
 
